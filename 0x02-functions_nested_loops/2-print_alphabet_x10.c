c449ce52a21052d19471cf665b1dca13a80b2afc
khadija <khadijakartouche368@gmail.com>
2023-03-21 18:11:05 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * main - check the code
- * Return: Always 0
+ * Description:print_alphabet_x10-function that print the alphabet 10 times
+ *
  */
+
 void print_alphabet_x10(void)
 {
 	char c;
