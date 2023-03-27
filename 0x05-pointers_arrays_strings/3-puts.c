a6c41ddcfd08d854aca014d0b4e80db0f2677017
khadija <khadijakartouche368@gmail.com>
2023-03-27 14:31:33 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+
+for(; *str != '\0'; str++)
+{
+putchar(*str);
+}
+
+putchar('\n');
+}
