3e3b9bb414d38d5bb8a4de99e0e723fd9b008023
khadija <khadijakartouche368@gmail.com>
2023-03-31 08:31:33 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void reverse_array(int *a, int n);
+int main(void)
 
 #endif
